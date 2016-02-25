da7b50b960f76ce0a19d04a9de29ed23b1322513
alexbruckner <bruckner.alex@gmail.com>
2016-02-25 21:12:54 +0100

    getting there. moved stage drawing of nodes into latticegames.js nodes

--- frontend/www/js/app.js
+++ frontend/www/js/app.js
@@ -35,17 +35,13 @@ angular.module('starter', ['ionic'])
 
 .controller('CreateLatticeController', function($scope, $http) {
 
-  var l = LatticeGames.Lattice("muh");
+  var lattice = LatticeGames.Lattice("Lattice");
 
-  var n1 = LatticeGames.Node();
-  var n2 = LatticeGames.Node();
+//  var n1 = LatticeGames.Node();
 
-  l.add(n1);
-  l.add(n2);
+//  lattice.add(n1);
 
-  n1.link(n2);
-
-  log(l);
+  log(lattice);
 
 //    $scope.latticeName= "Lattice";
 //
@@ -55,17 +51,17 @@ angular.module('starter', ['ionic'])
 //    var texts = {};
 //
 //
-//    var element = document.getElementById("createLattice");
-//    WIDTH = 600;
-//    HEIGHT = 400;
-//    var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
-//    element.appendChild(renderer.view);
-//
-//    // create the root of the scene graph
-//    var stage = new PIXI.Container();
-//
-//    // create a texture from an image path
-//    var texture = PIXI.Texture.fromImage('img/ionic.png');
+    var element = document.getElementById("createLattice");
+    WIDTH = 600;
+    HEIGHT = 400;
+    var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
+    element.appendChild(renderer.view);
+
+    // create the root of the scene graph
+    var stage = new PIXI.Container();
+
+    // create a texture from an image path
+    var texture = PIXI.Texture.fromImage('img/ionic.png');
 //
 //     function reset() {
 //
@@ -91,92 +87,45 @@ angular.module('starter', ['ionic'])
 //     }
 //
 //
-//    createBackground();
-//
-//    function createBackground() {
-//
-//        // create our little background friend..
-//        var background = new PIXI.Sprite();
-//
-//        // enable the background to be interactive... this will allow it to respond to mouse and touch events
-//        background.interactive = true;
-//
-//        // this button mode will mean the hand cursor appears when you roll over the background with your mouse
-//        background.buttonMode = true;
-//
-//        // make it a bit bigger, so it's easier to grab
-//        background.width = WIDTH;
-//        background.height = HEIGHT;
-//
-//        background.on('mousedown', backgroundMouseDown)
-//                  .on('touchstart', backgroundMouseDown);
-//
-//        function backgroundMouseDown (e) {
-//           x = renderer.plugins.interaction.eventData.data.global.x;
-//           y = renderer.plugins.interaction.eventData.data.global.y;
-//           createNode(x, y);
-//        }
-//
-//        // move the sprite to its designated position
-//        background.position.x = 0;
-//        background.position.y = 0;
-//
-//        // add it to the stage
-//        stage.addChild(background);
-//
-//    }
-//
-//    var nodeId = 1;
-//
-//    function createNode(x, y, id)
-//    {
-//        // create our little node friend..
-//        var node = new PIXI.Sprite(texture);
-//
-//        if (id) {
-//          node.id = id;
-//        } else {
-//          node.id = nodeId++;
-//        }
-//
-//        // enable the node to be interactive... this will allow it to respond to mouse and touch events
-//        node.interactive = true;
-//
-//        // this button mode will mean the hand cursor appears when you roll over the node with your mouse
-//        node.buttonMode = true;
-//
-//        // center the node's anchor point
-//        node.anchor.set(0.5);
-//
-//        // make it a bit bigger, so it's easier to grab
-//        node.scale.set(1);
-//
-//        // setup events
-//        node
-//            // events for drag start
-//            .on('mousedown', onDragStart)
-//            .on('touchstart', onDragStart)
-//            // events for drag end
-//            .on('mouseup', onDragEnd)
-//            .on('mouseupoutside', onDragEnd)
-//            .on('touchend', onDragEnd)
-//            .on('touchendoutside', onDragEnd)
-//            // events for drag move
-//            .on('mousemove', onDragMove)
-//            .on('touchmove', onDragMove);
-//
-//        // move the sprite to its designated position
-//        node.position.x = x;
-//        node.position.y = y;
-//
-//        // add it to the stage
-//        stage.addChild(node);
-//
-//        nodes[node.id] = node;
-//
-//        createText(node);
-//
-//    }
+    createBackground(stage);
+
+    function createBackground(stage) {
+
+        // create our little background friend..
+        var background = new PIXI.Sprite();
+
+        // enable the background to be interactive... this will allow it to respond to mouse and touch events
+        background.interactive = true;
+
+        // this button mode will mean the hand cursor appears when you roll over the background with your mouse
+        background.buttonMode = true;
+
+        // make it a bit bigger, so it's easier to grab
+        background.width = WIDTH;
+        background.height = HEIGHT;
+
+        background.on('mousedown', backgroundMouseDown)
+                  .on('touchstart', backgroundMouseDown);
+
+        function backgroundMouseDown (e) {
+           x = renderer.plugins.interaction.eventData.data.global.x;
+           y = renderer.plugins.interaction.eventData.data.global.y;
+           createNode(x, y);
+        }
+
+        // move the sprite to its designated position
+        background.position.x = 0;
+        background.position.y = 0;
+
+        // add it to the stage
+        stage.addChild(background);
+
+    }
+
+    function createNode(x, y) {
+        lattice.add(LatticeGames.Node(stage, texture, x, y));
+        log(lattice);
+    }
 //
 //    function createText(node) {
 //
@@ -262,105 +211,15 @@ angular.module('starter', ['ionic'])
 //      }
 //    }
 //
-//    requestAnimationFrame( animate );
-//
-//    function animate() {
-//
-//        requestAnimationFrame(animate);
-//
-//        // render the stage
-//        renderer.render(stage);
-//    }
-//
-//    var currentNode;
-//    function onDragStart(event)
-//    {
-//
-//       // check dblclick
-//        if (this.lastClickTime) {
-//
-//         if (Date.now() - this.lastClickTime < 200) {
-//             currentNode = this;
-//             return;
-//         }
-//
-//       }
-//       this.lastClickTime = Date.now();
-//
-//
-//
-//        // store a reference to the data
-//        // the reason for this is because of multitouch
-//        // we want to track the movement of this particular touch
-//        this.data = event.data;
-//        this.alpha = 0.5;
-//        this.dragging = true;
-//    }
-//
-//    function onDragEnd()
-//    {
-//        this.alpha = 1;
-//
-//        this.dragging = false;
-//
-//        // set the interaction data to null
-//        this.data = null;
-//
-//
-//        if (currentNode) {
-//
-//          x2 = renderer.plugins.interaction.eventData.data.global.x;
-//          y2 = renderer.plugins.interaction.eventData.data.global.y
-//
-//          for (i in nodes) {
-//            if (x2 > nodes[i].position.x - nodes[i].width/2 && x2 < nodes[i].position.x + nodes[i].width/2 &&
-//                y2 > nodes[i].position.y - nodes[i].height/2 && y2 < nodes[i].position.y + nodes[i].height/2) {
-//                  // fine
-//                  linkNodes(currentNode, nodes[i]);
-//                } else {
-//                  stage.removeChild(lines[currentNode.id]);
-//                  delete lines[currentNode.id];
-//                }
-//          }
-//
-//          currentNode = null;
-//        }
-//
-//    }
-//
-//    function onDragMove()
-//    {
-//        if (currentNode) {
-//            x2 = renderer.plugins.interaction.eventData.data.global.x;
-//            y2 = renderer.plugins.interaction.eventData.data.global.y
-//            createLine(currentNode, x2, y2);
-//        }
-//
-//        if (this.dragging)
-//        {
-//            var newPosition = this.data.getLocalPosition(this.parent);
-//
-//            if (newPosition.x < this.width/2 ||
-//                newPosition.y < this.height/2 ||
-//                newPosition.x > WIDTH - this.width/2 ||
-//                newPosition.y > HEIGHT - this.height/2) {
-//              return;
-//            }
-//
-//            this.position.x = newPosition.x;
-//            this.position.y = newPosition.y;
-//
-//            moveLink(this);
-//            createText(this);
-//
-//        }
-//    }
-//
-//    $scope.logLinks = function () {
-//      console.log(links);
-//    }
-//
-//
+    requestAnimationFrame( animate );
+
+    function animate() {
+
+        requestAnimationFrame(animate);
+
+        // render the stage
+        renderer.render(stage);
+    }
 //
 //     $scope.loadLattices = function () {
 //        $http({
--- frontend/www/js/latticegames.js
+++ frontend/www/js/latticegames.js
@@ -38,11 +38,12 @@ var Lattice = function (name) {
   return this;
 }
 
-var Node = function () {
+var Node = function (stage, texture, x, y) {
   this.id = null;
   this.name = null;
   this.lattice = null;
   this.neighbours = [];
+  this.graphicsNode = null;
   this.link = function (otherNode, dontDoOther) {
     if (this.lattice === otherNode.lattice) {
       if (this === otherNode) {
@@ -62,6 +63,130 @@ var Node = function () {
   this.toString = function(){
     return "Node {id: {0}, name: {1}, neighbours: {2}}".format(this.id, this.name, this.neighbours.map(function (neighbour) {return neighbour.name;}).join(", "));
   }
+  this.graphics = function(stage, texture, x, y) {
+          // create our little node friend..
+            var node = new PIXI.Sprite(texture);
+
+            // enable the node to be interactive... this will allow it to respond to mouse and touch events
+            node.interactive = true;
+
+            // this button mode will mean the hand cursor appears when you roll over the node with your mouse
+            node.buttonMode = true;
+
+            // center the node's anchor point
+            node.anchor.set(0.5);
+
+            // make it a bit bigger, so it's easier to grab
+            node.scale.set(1);
+
+            // setup events
+            node
+                // events for drag start
+                .on('mousedown', onDragStart)
+                .on('touchstart', onDragStart)
+                // events for drag end
+                .on('mouseup', onDragEnd)
+                .on('mouseupoutside', onDragEnd)
+                .on('touchend', onDragEnd)
+                .on('touchendoutside', onDragEnd)
+                // events for drag move
+                .on('mousemove', onDragMove)
+                .on('touchmove', onDragMove);
+
+            // move the sprite to its designated position
+            node.position.x = x;
+            node.position.y = y;
+
+            // add it to the stage
+            stage.addChild(node);
+            this.graphicsNode = node;
+
+
+
+      function onDragStart(event)
+      {
+
+       // check dblclick
+        if (this.lastClickTime) {
+
+            if (Date.now() - this.lastClickTime < 200) {
+               currentNode = this;
+               return;
+            }
+
+          }
+         this.lastClickTime = Date.now();
+
+          // store a reference to the data
+          // the reason for this is because of multitouch
+          // we want to track the movement of this particular touch
+          this.data = event.data;
+          this.alpha = 0.5;
+          this.dragging = true;
+      }
+
+      function onDragEnd()
+      {
+          this.alpha = 1;
+
+          this.dragging = false;
+
+          // set the interaction data to null
+          this.data = null;
+
+
+    //                    if (currentNode) {
+    //
+    //                      x2 = renderer.plugins.interaction.eventData.data.global.x;
+    //                      y2 = renderer.plugins.interaction.eventData.data.global.y
+    //
+    //                      for (i in nodes) {
+    //                        if (x2 > nodes[i].position.x - nodes[i].width/2 && x2 < nodes[i].position.x + nodes[i].width/2 &&
+    //                            y2 > nodes[i].position.y - nodes[i].height/2 && y2 < nodes[i].position.y + nodes[i].height/2) {
+    //                              // fine
+    //                              linkNodes(currentNode, nodes[i]);
+    //                            } else {
+    //                              stage.removeChild(lines[currentNode.id]);
+    //                              delete lines[currentNode.id];
+    //                            }
+    //                      }
+    //
+    //                      currentNode = null;
+    //                    }
+
+      }
+
+      function onDragMove()
+      {
+    //      if (currentNode) {
+    //          x2 = renderer.plugins.interaction.eventData.data.global.x;
+    //          y2 = renderer.plugins.interaction.eventData.data.global.y
+    //          createLine(currentNode, x2, y2);
+    //      }
+
+          if (this.dragging)
+          {
+              var newPosition = this.data.getLocalPosition(this.parent);
+
+              if (newPosition.x < this.width/2 ||
+                  newPosition.y < this.height/2 ||
+                  newPosition.x > WIDTH - this.width/2 ||
+                  newPosition.y > HEIGHT - this.height/2) {
+                return;
+              }
+
+              this.position.x = newPosition.x;
+              this.position.y = newPosition.y;
+
+    //          moveLink(this);
+    //          createText(this);
+
+          }
+      }
+
+  }
+
+  this.graphics(stage, texture, x, y);
   return this;
 }
 
@@ -71,8 +196,8 @@ LatticeGames = {
       return new Lattice(name);
     },
 
-    Node: function (name) {
-      return new Node(name);
+    Node: function (stage, texture, x, y) {
+      return new Node(stage, texture, x, y);
     }
 
 }
